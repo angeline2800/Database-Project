7dc80164399d763940478ca4ddd05cab9a8e0aea
amywong110 <107854660+amywong110@users.noreply.github.com>
2022-06-24 20:38:38 +0800

    Add files via upload

---
+++ adminAddOrchard.php
@@ -0,0 +1,126 @@
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "tmf2034_Group4_Project";
+					
+	//Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	
+	$orchard_add = "";
+	$orchard_location = "";
+	$userID = "";
+	
+	$errorMessage="";
+	$sucessMessage="";
+	
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$orchard_add = $_POST["orchard_add"];
+		$orchard_location = $_POST["orchard_location"];
+		$userID = $_POST["userID"];
+		
+		do {
+			if ( empty($orchard_add) || empty($orchard_location) || empty($userID) )
+			{
+				$errorMessage = "All the fields are required";
+				break;
+			}
+			
+			// add new user to database
+			$sqlOrchard = "INSERT INTO orchard (orchard_add, orchard_location, userID)"
+			. "VALUES ('$orchard_add', '$orchard_location', '$userID')";
+			$result = $conn->query($sqlOrchard);
+			
+			if (!$result) {
+				$errorMessage = "Invalid query: " . $conn->error;
+				break;
+			}
+			
+			$orchard_add = "";
+			$orchard_location = "";
+			$userID = "";
+			
+			$sucessMessage = "Orchard added successfully!";
+			
+			//header("location: /tmf2034/DisplayUser.php");
+			//exit;
+			
+		}while(false);
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Admin Add Orchard</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"></link>
+	<script src ="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <center><h1>Orchard</h1></center>
+	
+	<div class = "container my-5">
+        <h2>New Orchard</h2>
+		
+		<?php
+		if(!empty($errorMessage)){
+			echo "
+			<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+				<strong>$errorMessage</strong>
+				<button type='button' class='btn-close' data-bs-dismiss='alert' atia-label='Close' </button>
+			</div>
+			";
+		}
+		?>
+		<form method="post">
+		<div class="row mb-3">
+			<label class="col-sm-3 col-form-label">Orchard Address</label>
+				<div class="col-sm-6">
+					<input type="text" class="form-control" name="orchard_add" placeholder="enter orchard address"  value="<?php echo $orchard_add; ?>">
+				</div>
+		</div>
+		<div class="row mb-3">
+			<label class="col-sm-3 col-form-label">Orchard Location</label>
+				<div class="col-sm-6">
+					<input type="text" class="form-control" name="orchard_location" placeholder="enter orchard location" value="<?php echo $orchard_location; ?>">
+				</div>
+		</div>
+		<div class="row mb-3">
+			<label class="col-sm-3 col-form-label">User ID</label>
+				<div class="col-sm-6">
+					<input type="text" class="form-control" name="userID" placeholder="enter user ID" value="<?php echo $userID; ?>">
+				</div>
+		</div>
+		
+		
+		<?php
+			if(!empty($sucessMessage)){
+			echo "
+			<div class='row mb-3'>
+				<div class='offset -sm3 col-sm-6'>
+					<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+						<strong>$sucessMessage</strong>
+						<button type='button' class='btn-close' data-bs-dismiss='alert' atia-label='Close' </button>
+					</div>
+				</div>
+			</div>
+			";
+			}
+		?>
+		
+		<div class="row mb-3">
+			<div class="offset-sm-3 col-sm-3 d-grid">
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</div>
+			<div class="col-sm-3 d-grid">
+				<a class="btn btn-outline-primary" href="/tmf2034/project/adminOrchard.php" role="button">Cancel/Back</a>
+			</div>
+		</div>
+		
+	</div>
+			
+</body>
+</html>
---
+++ adminOrchard.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Admin</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"></link>
+</head>
+<body>
+    <center><h1>Orchard</h1></center>
+	
+	<div class = "container my-5">
+        <h2>List of orchard</h2>
+		<a class="btn btn-primary" href="/tmf2034/project/adminAddOrchard.php" role="button">Add - New Orchard</a>
+        <br>
+		<table class="table">
+			<thead>
+				<tr>
+					<th>Orchard ID</th>
+					<th>Orchard Address</th>
+					<th>Orchard Location</th>
+					<th>User ID</th>
+					
+				<tr>
+			</thead>
+			<tbody>
+				<?php
+					$servername = "localhost";
+					$username = "root";
+					$password = "";
+					$dbname = "tmf2034_Group4_Project";
+					
+					//Create connection
+					$conn = new mysqli($servername, $username, $password, $dbname);
+					
+					//Check connection
+					if($conn->connect_error)
+					{			
+						die("connection failed: ". $conn->connect_error);
+					}
+					
+					//read all row from database table orchard
+					$sqlOrchard= "SELECT * FROM orchard";
+					$result = $conn->query($sqlOrchard);
+					
+					if(!$result){
+						die("Invalid query: " . $conn->error);
+					}
+					
+					//read data of each row
+					while($row = $result->fetch_assoc())
+					{
+						echo "
+						<tr>
+							<td>$row[orchardID]</td>
+							<td>$row[orchard_add]</td>
+							<td>$row[orchard_location]</td>
+							<td>$row[userID]</td>
+							
+							<td>
+								<a class='btn btn-primary btn-sm' href='/tmf2034/update.php?userID=$row[userID]'>Update</a>
+								<a class='btn btn-primary btn-sm' href='/tmf2034/delete.php?userID=$row[userID]'>Delete</a>
+							</td>	
+						</tr>
+						";
+					}
+				?>				
+				
+			</tbody>
+
+</body>
+</html>
