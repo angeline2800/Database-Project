1c6f714f08588e4693d73a7bf5e8ee9600edae4d
amywong110 <107854660+amywong110@users.noreply.github.com>
2022-06-26 03:17:40 +0800

    Add files via upload

---
+++ adminDeleteUser.php
@@ -0,0 +1,14 @@
+<?php
+
+	include "dbConnection.php";
+	$userID = $_GET['userID'];
+	$sql = "DELETE FROM `user` WHERE userID = $userID";
+	$result = mysqli_query($conn, $sql);
+	
+	if($result){
+			header("Location: admin.php?msg=Record deleted successfully");
+	}
+	else{
+		echo "Failed: " . mysqli_error($conn);
+	}
+?>
