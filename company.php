cd38366e2f275018949cb6a262fe6096d65c3f0c
May.W <75057543+SzeeMei@users.noreply.github.com>
2022-06-26 16:53:06 +0800

    fixed show record

--- company.php
+++ company.php
@@ -2,39 +2,6 @@
 
 session_start();
 
-function display_company()
-{
-      global $conn;
-            
-          if($_SERVER["REQUEST_METHOD"]=="POST") 
-          {
-
-      $query = "SELECT * FROM company WHERE userID= $userID ";
-      $result = mysqli_query($conn, $query);
-
-      if($result-> num_rows > 0){
-          while($row = mysqli_fetch_array($result))
-          {
-            echo"<tr>";
-            echo "<td>" .$row['userID']."</td>";
-            echo "<td>" .$row['orchardID']."</td>";
-            echo "<td>" .$row['orchard_add']."</td>";
-            echo "<td>" .$row['orchard_location']."</td>";
-            echo"</tr>";
-            }      
-      }
-
-      else
-      {
-       
-          echo "Error creating Table User: " . mysqli_error($conn);
-        
-      }
-
-    }
-  }
- 
-
 if(isset($_POST['Logout']))
 {
   header('location:UserLogout.php');
@@ -75,14 +42,29 @@ if(isset($_POST['Logout']))
                     <th>Orchard Locatiion</th>
                 </tr>
              
-                <tr>
-                    <th>TRY</th>
-                    <th>TRY</th>
-                    <th>TRY</th>
-                    <th>TRY</th>
-
-                </tr>
-                   <?php display_company();?>
+                <?php 
+                  if(isset($_SESSION['user']['userID'])) 
+                  {
+                    $userid = $_SESSION['user']['userID'];
+                    $query = "SELECT * FROM orchard WHERE userID= $userid ";
+                    $result = mysqli_query($conn, $query);
+  
+                    if(mysqli_num_rows($result) > 0){
+                      while($row = mysqli_fetch_array($result))
+                      {
+                        echo"<tr>";
+                        echo "<td>" .$row['userID']."</td>";
+                        echo "<td>" .$row['orchardID']."</td>";
+                        echo "<td>" .$row['orchard_add']."</td>";
+                        echo "<td>" .$row['orchard_location']."</td>";
+                        echo"</tr>";
+                      } 
+                    }
+                    else{
+                        echo "<h2>No Records Found!</h2>";
+                    }
+                  }  
+                ?>
             </table> 
           <button type="submit" class="btn" name="Logout">Logout</button>
             </center>
