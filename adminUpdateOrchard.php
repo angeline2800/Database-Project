2e200bc04ba68dbee97d5c4753c563810a4d14ad
amywong110 <107854660+amywong110@users.noreply.github.com>
2022-06-26 23:21:38 +0800

    Update adminUpdateOrchard.php

--- adminUpdateOrchard.php
+++ adminUpdateOrchard.php
@@ -33,17 +33,17 @@
 	<header class="header-border">
 		<div class="header-content">
 	
-      <h1 class="slogan"><span><img src="photo/headerLogo.png" alt="System - Logo" height="90"></span>TREE PROFILING MANAGEMENT SYSTEM</h1>
-        <div class="slogan2">
-         <b> <p>YOUR GOOD HELPER IN LIFE</p></b>
-        </div>
-    </div>
-  </header>
+			<h1 class="slogan"><span><img src="photo/headerLogo.png" alt="System - Logo" height="90"></span>TREE PROFILING MANAGEMENT SYSTEM</h1>
+			<div class="slogan2">
+				<b> <p>YOUR GOOD HELPER IN LIFE</p></b>
+			</div>
+		</div>
+	</header>
 
-  <div class="header">
-	<h2>Update Orchard Information</h2>
-</div>
-<div class="content">
+	<div class="header">
+		<h2>Update Orchard Information</h2>
+	</div>
+	<div class="content">
 			<center><h3>Click update after changing any information</h3></center>
 			<div class="updateOrchard">
 			<form action="" method="post">
@@ -52,27 +52,32 @@
 					<input type = "text" name="orchard_add" id="orchard_add" value="<?php echo $orchard_add; ?>">
 					<label>Orchard Location</label>
 					<input type = "text" name="orchard_location" id="orchard_location" value="<?php echo $orchard_location; ?>">
+					<br/>
+					<label>User ID</label>
+					<input type = "text" name="userID" id="userID" value="<?php echo $userID; ?>">
 
 					<input type="submit"class="updateSubmit" name="edit" value="Update" />
 					<a href="adminOrchard.php"><button>Back to Orchard List</button></a>			
 				</div>
 			</form>
-		</div>	
+			</div>	
+		</div>
 </body>
 
 	<?php
-		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		if(isset($_POST["update"]))
-	{
-		mysqli_query($conn, "UPDATE orchard SET orchard_add='$_POST[orchard_add]', orchard_location='$_POST[orchard_location]', userID='$_POST[userID]' where orchardID=$orchardID");
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+		{
+			if(isset($_POST["edit"]))
+			{
+				mysqli_query($conn, "UPDATE orchard SET orchard_add='$_POST[orchard_add]', orchard_location='$_POST[orchard_location]', userID='$_POST[userID]' where orchardID=$orchardID");
 	
-		?><script type="text/javascript">window.location="adminOrchard.php"</script><?php
-	}
-	else
-	{
-		header("location: adminOrchard.php");
-	}
-}
+				?><script type="text/javascript">window.location="adminOrchard.php"</script><?php
+			}
+			else
+			{
+			header("location: adminOrchard.php");
+			}
+		}
 	?>
 	
 
