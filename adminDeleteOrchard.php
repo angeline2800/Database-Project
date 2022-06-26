31869d4b0f91c471b01867db455507874fd20fbe
amywong110 <107854660+amywong110@users.noreply.github.com>
2022-06-26 13:41:58 +0800

    Update adminDeleteOrchard.php

--- adminDeleteOrchard.php
+++ adminDeleteOrchard.php
@@ -1,28 +1,15 @@
+
 <?php
 
-	if( isset($_GET["orchardID"]))
-	{
-		$orchardID = $_GET["orchardID"];
-		
-		$servername = "localhost";
-		$username = "root";
-		$password = "";
-		$dbname = "tmf2034_Group4_Project";
-					
-		//Create connection
-		$conn = new mysqli($servername, $username, $password, $dbname);
+	include "dbConnection.php";
+	$orchardID = $_GET['orchardID'];
+	$sql = "DELETE FROM `orchard` WHERE orchardID = $orchardID";
+	$result = mysqli_query($conn, $sql);
 	
-		// Check connection
-		if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-}
-		$sql = "DELETE FROM orchard WHERE orchardID = $orchardID";
-		$conn->query($sql);
-		
+	if($result){
+			header("Location: adminOrchard.php?msg=Record deleted successfully");
 	}
-	
-	
-header("location: /tmf2034/project/adminOrchard.php");
-exit;
-
+	else{
+		echo "Failed: " . mysqli_error($conn);
+	}
 ?>
