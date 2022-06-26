6fe652c1d4cb37a7ce5b18ad873d01d339eeca8c
amywong110 <107854660+amywong110@users.noreply.github.com>
2022-06-26 12:22:45 +0800

    Update adminAddOrchard.php

--- adminAddOrchard.php
+++ adminAddOrchard.php
@@ -42,8 +42,8 @@
 			
 			$sucessMessage = "Orchard added successfully!";
 			
-			//header("location: /tmf2034/DisplayUser.php");
-			//exit;
+			header("location: adminOrchard.php");
+			exit;
 			
 		}while(false);
 	}
@@ -56,11 +56,24 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Admin Add Orchard</title>
+	<title>Orchards | Administration | Tree Profiling Management System</title>
 	<link rel="shortcut icon" href="photo/tree.ico" />
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"></link>
-	<script src ="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+	<link rel="stylesheet" href="CSS/worker.css">
+	<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"></link> -->
+	
+	<!--<script src ="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>-->
 </head>
 <body>
+<header class="header-border">
+		<div class="header-content">
+		
+			<h1 class="slogan"><span><img src="photo/headerLogo.png" alt="System - Logo" height="90"></span>TREE PROFILING MANAGEMENT SYSTEM</h1>
+				<div class="slogan2">
+					<b> <p>YOUR GOOD HELPER IN LIFE</p></b>
+				</div>
+		</div>
+</header>
+
     <center><h1>Orchard</h1></center>
 	
 	<div class = "container my-5">
@@ -77,51 +90,51 @@
 		}
 		?>
 		<form method="post">
-		<div class="row mb-3">
-			<label class="col-sm-3 col-form-label">Orchard Address</label>
-				<div class="col-sm-6">
-					<input type="text" class="form-control" name="orchard_add" placeholder="enter orchard address"  value="<?php echo $orchard_add; ?>">
-				</div>
-		</div>
-		<div class="row mb-3">
-			<label class="col-sm-3 col-form-label">Orchard Location</label>
-				<div class="col-sm-6">
-					<input type="text" class="form-control" name="orchard_location" placeholder="enter orchard location" value="<?php echo $orchard_location; ?>">
-				</div>
-		</div>
-		<div class="row mb-3">
-			<label class="col-sm-3 col-form-label">User ID</label>
-				<div class="col-sm-6">
-					<input type="text" class="form-control" name="userID" placeholder="enter user ID" value="<?php echo $userID; ?>">
-				</div>
-		</div>
+			<div class="row mb-3">
+				<label class="col-sm-3 col-form-label">Orchard Address</label>
+					<div class="col-sm-6">
+						<input type="text" class="form-control" name="orchard_add" placeholder="enter orchard address"  value="<?php echo $orchard_add; ?>">
+					</div>
+			</div>
+			<div class="row mb-3">
+				<label class="col-sm-3 col-form-label">Orchard Location</label>
+					<div class="col-sm-6">
+						<input type="text" class="form-control" name="orchard_location" placeholder="enter orchard location" value="<?php echo $orchard_location; ?>">
+					</div>
+			</div>
+			<div class="row mb-3">
+				<label class="col-sm-3 col-form-label">User ID</label>
+					<div class="col-sm-6">
+						<input type="text" class="form-control" name="userID" placeholder="enter user ID" value="<?php echo $userID; ?>">
+					</div>
+			</div>
 		
 		
-		<?php
-			if(!empty($sucessMessage)){
-			echo "
-			<div class='row mb-3'>
-				<div class='offset -sm3 col-sm-6'>
-					<div class='alert alert-warning alert-dismissible fade show' role='alert'>
-						<strong>$sucessMessage</strong>
-						<button type='button' class='btn-close' data-bs-dismiss='alert' atia-label='Close' </button>
+			<?php
+				if(!empty($sucessMessage)){
+				echo "
+				<div class='row mb-3'>
+					<div class='offset -sm3 col-sm-6'>
+						<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+							<strong>$sucessMessage</strong>
+							<button type='button' class='btn-close' data-bs-dismiss='alert' atia-label='Close' </button>
+						</div>
 					</div>
 				</div>
-			</div>
-			";
-			}
-		?>
+				";
+				}
+			?>
 		
-		<div class="row mb-3">
-			<div class="offset-sm-3 col-sm-3 d-grid">
-				<button type="submit" class="btn btn-primary">Submit</button>
-			</div>
-			<div class="col-sm-3 d-grid">
-				<a class="btn btn-outline-primary" href="/tmf2034/project/adminOrchard.php" role="button">Cancel/Back</a>
-			</div>
-		</div>
+			<div class="row mb-3">
+				<div class="offset-sm-3 col-sm-3 d-grid">
+					<button type="submit" class="btn btn-primary">Submit</button>
+				
+					<a class="btn btn-outline-primary" href="adminOrchard.php" role="button">Cancel/Back</a>
+				
+				</div>
 		
-	</div>
+			</div>
+		</form>
 			
 </body>
 </html>
