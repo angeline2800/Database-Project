e09e1af431b25bd3525ba108453c49234fdea4f3
amywong110 <107854660+amywong110@users.noreply.github.com>
2022-06-26 13:51:28 +0800

    Update adminAddBlock.php

--- adminAddBlock.php
+++ adminAddBlock.php
@@ -46,67 +46,81 @@
     <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Admin Add Block</title>
+	<title>Orchard | Administration | Tree Profiling Management System</title>
 	<link rel="shortcut icon" href="photo/tree.ico" />
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"></link>
+	<link rel="stylesheet" href="CSS/worker.css">
+	<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"></link> -->
+	
+	<!--link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"></link>-->
 	<script src ="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <center><h1>Block</h1></center>
-	
-	<div class = "container my-5">
-        <h2>New Block</h2>
+	<header class="header-border">
+		<div class="header-content">
 		
-		<?php
-		if(!empty($errorMessage)){
-			echo "
-			<div class='alert alert-warning alert-dismissible fade show' role='alert'>
-				<strong>$errorMessage</strong>
-				<button type='button' class='btn-close' data-bs-dismiss='alert' atia-label='Close' </button>
+			<h1 class="slogan"><span><img src="photo/headerLogo.png" alt="System - Logo" height="90"></span>TREE PROFILING MANAGEMENT SYSTEM</h1>
+			<div class="slogan2">
+				<b> <p>YOUR GOOD HELPER IN LIFE</p></b>
 			</div>
-			";
-		}
-		?>
-		<form method="post">
-		<div class="row mb-3">
-			<label class="col-sm-3 col-form-label">Price</label>
-				<div class="col-sm-6">
-					<input type="text" class="form-control" name="Price" placeholder="enter Price RMxxxx"  value="<?php echo $Price; ?>">
-				</div>
 		</div>
-		<div class="row mb-3">
-			<label class="col-sm-3 col-form-label">Orchard ID</label>
-				<div class="col-sm-6">
-					<input type="text" class="form-control" name="orchardID" placeholder="enter orchard ID" value="<?php echo $orchardID; ?>">
-				</div>
-		</div>
+	</header>
+	
+		<center><h1>Block</h1></center>
 		
-		
-		<?php
-			if(!empty($sucessMessage)){
-			echo "
-			<div class='row mb-3'>
-				<div class='offset -sm3 col-sm-6'>
+		<div class = "container my-5">
+			<h2>New Block</h2>
+			
+			<?php
+				if(!empty($errorMessage)){
+					echo "
 					<div class='alert alert-warning alert-dismissible fade show' role='alert'>
-						<strong>$sucessMessage</strong>
+						<strong>$errorMessage</strong>
 						<button type='button' class='btn-close' data-bs-dismiss='alert' atia-label='Close' </button>
 					</div>
+					";
+				}
+			?>
+			
+			<form method="post">
+			
+				<div class="row mb-3">
+					<label class="col-sm-3 col-form-label">Price</label>
+						<div class="col-sm-6">
+							<input type="text" class="form-control" name="Price" placeholder="enter Price RMxxxx"  value="<?php echo $Price; ?>">
+						</div>
 				</div>
-			</div>
-			";
-			}
-		?>
-		
-		<div class="row mb-3">
-			<div class="offset-sm-3 col-sm-3 d-grid">
-				<button type="submit" class="btn btn-primary">Submit</button>
-			</div>
-			<div class="col-sm-3 d-grid">
-				<a class="btn btn-outline-primary" href="adminBlock.php" role="button">Cancel/Back</a>
-			</div>
+				
+				<div class="row mb-3">
+					<label class="col-sm-3 col-form-label">Orchard ID</label>
+						<div class="col-sm-6">
+							<input type="text" class="form-control" name="orchardID" placeholder="enter orchard ID" value="<?php echo $orchardID; ?>">
+						</div>
+				</div>
+			
+				<?php
+					if(!empty($sucessMessage)){
+					echo "
+					<div class='row mb-3'>
+						<div class='offset -sm3 col-sm-6'>
+							<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+								<strong>$sucessMessage</strong>
+								<button type='button' class='btn-close' data-bs-dismiss='alert' atia-label='Close' </button>
+							</div>
+						</div>
+					</div>
+					";
+					}
+				?>
+			
+				<div class="row mb-3">
+					<div class="offset-sm-3 col-sm-3 d-grid">
+						<button type="submit" class="btn btn-primary">Submit</button>
+				
+						<a class="btn btn-outline-primary" href="adminBlock.php" role="button">Cancel/Back</a>
+					</div>
+				</div>
+			</form>
 		</div>
-		
-	</div>
 			
 </body>
 </html>
