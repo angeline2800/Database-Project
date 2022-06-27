338c61e92c7be02cdc7df8a0f17928dde62fe658
amywong110 <107854660+amywong110@users.noreply.github.com>
2022-06-27 10:10:30 +0800

    Update adminUpdateOrchard.php

--- adminUpdateOrchard.php
+++ adminUpdateOrchard.php
@@ -1,48 +1,19 @@
 <?php
 	include "dbConnection.php";
 	
-	$orchard_add = "";
-	$orchard_location= "";
-	$userID= "";
+	$orchardID = $_GET['orchardID'];
 	
-	$errorMessage="";
-	$sucessMessage="";
+	$orchard_add="";
+	$orchard_location="";
+	$userID="";
 	
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-		$orchard_add = $_POST["orchard_add"];
-		$orchard_location = $_POST["orchard_location"];
-		$userID = $_POST["userID"];
-		
-		do {
-			if ( empty($orchard_add) || empty($orchard_location) || empty($userID) )
-			{
-				$errorMessage = "All the fields are required";
-				break;
-			}
-			
-			// add new orchard to database
-			$sql = "INSERT INTO orchard (orchard_add, orchard_location, userID)"
-			. "VALUES ('$orchard_add', '$orchard_location', '$userID')";
-			$result = $conn->query($sql);
-			
-			if (!$result) {
-				$errorMessage = "Invalid User ID, Please input the valid User ID!";
-				break;
-			}
-			
-			$orchard_add = "";
-			$orchard_location= "";
-			$userID= "";
-			
-			$sucessMessage = "Orchard added successfully!";
-			
-			header("location: adminOrchard.php ");
-			exit;
-			
-		}while(false);
+	$result=mysqli_query($conn, "select * from orchard where orchardID=$orchardID");
+	while($row = mysqli_fetch_array($result))
+	{
+		$orchard_add = $row["orchard_add"];
+		$orchard_location = $row["orchard_location"];
+		$userID = $row["userID"];
 	}
-	
-
 ?>
 
 <!DOCTYPE html>
@@ -51,71 +22,63 @@
     <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<title>Orchard | Administration | Tree Profiling Management System</title>
 	<link rel="shortcut icon" href="photo/tree.ico" />
 	<link rel="stylesheet" href="CSS/block.css">
-	
+
 </head>
 <body>
 
-<header class="header-border">
-    <div class="header-content">
+	<header class="header-border">
+		<div class="header-content">
 	
-      <h1 class="slogan"><span><img src="photo/headerLogo.png" alt="System - Logo" height="90"></span>TREE PROFILING MANAGEMENT SYSTEM</h1>
-        <div class="slogan2">
-         <b> <p>YOUR GOOD HELPER IN LIFE</p></b>
-        </div>
-    </div>
-  </header>
-
-  <div class="header">
-	<h2>Add A New Orchard</h2>
-</div>
-
-<div class="content">
-<center><h3>Complete The Form Below To Add A New Orchard.</h3></center>
-	<div class="addNewOrchard">
-
-			<?php
-				if(!empty($errorMessage)){
-					echo "
-					<div class='alert alert-warning alert-dismissible fade show' role='alert'>
-						<strong>$errorMessage</strong>
-						
-					</div>
-					";
-					}
-			?>
-
-			<form action="" method="post">
-			<div class="insertOrchard">
-				<label>Orchard Address</label>
-				<input type = "text" name="orchard_add" placeholder="Enter address">
-				<label>Orchard Location</label>
-				<input type = "text" name="orchard_location" placeholder="Enter orchard location"><br/>
-				<label>User ID</label>
-				<input type = "text" name="userID" placeholder="Enter user ID">
-
-				<?php
-					if(!empty($sucessMessage)){
-					echo "
-					<div class='row mb-3'>
-						<div class='offset -sm3 col-sm-6'>
-							<div class='alert alert-warning alert-dismissible fade show' role='alert'>
-								<strong>$sucessMessage</strong>
-								
-							</div>
-						</div>
-					</div>
-					";
-					}
-				?>
-
-				<input type="submit"class="addSubmit" name="insert" value="Add" />
-				<a href="adminOrchard.php"><button>Back to Orchard List</button></a>
+			<h1 class="slogan"><span><img src="photo/headerLogo.png" alt="System - Logo" height="90"></span>TREE PROFILING MANAGEMENT SYSTEM</h1>
+			<div class="slogan2">
+				<b> <p>YOUR GOOD HELPER IN LIFE</p></b>
 			</div>
+		</div>
+	</header>
+
+	<div class="header">
+		<h2>Update Orchard Information</h2>
+	</div>
+	<div class="content">
+			<center><h3>Click update after changing any information</h3></center>
+			<div class="updateOrchard">
+			<form action="" method="post">
+				<div class="editOrchard">
+					<label>Orchard Address</label>
+					<input type = "text" name="orchard_add" id="orchard_add" value="<?php echo $orchard_add; ?>">
+					<label>Orchard Location</label>
+					<input type = "text" name="orchard_location" id="orchard_location" value="<?php echo $orchard_location; ?>">
+					<br/>
+					<label>User ID</label>
+					<input type = "text" name="userID" id="userID" value="<?php echo $userID; ?>">
+
+					<input type="submit"class="updateSubmit" name="edit" value="Update" />
+					<a href="adminOrchard.php"><button>Back to Orchard List</button></a>			
+				</div>
 			</form>
-		</div>	
-	</div>		
+			</div>	
+		</div>
 </body>
+
+	<?php
+		if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+		{
+			if(isset($_POST["edit"]))
+			{
+				mysqli_query($conn, "UPDATE orchard SET orchard_add='$_POST[orchard_add]', orchard_location='$_POST[orchard_location]', userID='$_POST[userID]' where orchardID=$orchardID");
+	
+				?><script type="text/javascript">window.location="adminOrchard.php"</script><?php
+			}
+			else
+			{
+			header("location: adminOrchard.php");
+			}
+		}
+	?>
+	
+
 </html>
